0fecb1c79e5563fe50559c767c7704eb0cb868e2
sjl4365 <113270140+sjl4365@users.noreply.github.com>
2026-02-01 05:17:04 -0500

    added another work experience

--- src/Aboutme.jsx
+++ src/Aboutme.jsx
@@ -2,13 +2,14 @@
 function Aboutme(){
     return(
         <div className="AboutMe">
-            <p>I am a HBSc Computer Science Co-op Student at the York University.
+            <p>I am a BSc Computer Science Co-op Student at the York University.
             I am someone who embrances new challenges as an opportunities to grow, and I am excited
             to be part of this journey in technological innovation.
             In my spare time, I enjoy activiites like swimming, where I was a 
             competitive swimmer in a club for 10 years.
         </p>
-        <p>Intereted in software enginner internship opportunities for Winter 2026</p>
+        <p>I am actively seeking Software Test Engineering opportunities where I can contribute to innovative teams, 
+            continue learning, and apply hands on technical skills in a real-world environment.</p>
     
         </div>
         
--- src/Workexperience.jsx
+++ src/Workexperience.jsx
@@ -1,87 +1,109 @@
 /*src/Workexperience.jsx */
+/*src/Workexperience.jsx */
 function Workexperience(){
 
-
     return(
-    
+        <div className="RelevantWorkExperience">
+            <h2 className="WorkExperience">Relevant Work Experience</h2>
+            <div>
+                <div className="JobFour">
+                    <h3 className="MOT">Quality Engineer @ TD Bank</h3>
+                    <h4 className="DateFour">Jan 2026 - Apr 2026</h4>
+                </div>
+                <ul className="listOne">
+                    <li>
+                    Develop and execute automated test scripts using Selenium WebDriver and Java to validate EasyWeb platform functionality, ensuring 
+                    cross browser compatibility and performance across multiple test environments
+                    </li>
+                    <li>
+                        Implement test automation frameworks within CI/CD pipelines using Jenkins and Git to enable continuous testing,
+                        integration, and delivery practices for faster release cycles
+                    </li>
+                    <li>
+                        Create and maintain comprehensive test plans, test cases, and defect reports in JIRA while performing Adhoc testing
+                        and collaborating with cross functional development teams
+                    </li>
+                </ul>
+            </div>
 
-    <div className="RelevantWorkExperience">
-        <h2 className="WorkExperience" >Relevant Work Experience</h2>
-        <div>
-            <div className="JobFour">
-                <h3 className="MOT">Software Developer @ Ontario Ministry of Transportation</h3>
-                <h4 className="DateFour">Jan 2025 - Apr 2025 </h4>
+            <div>
+                <div className="JobFour">
+                    <h3 className="MOT">Software Developer @ Ontario Ministry of Transportation</h3>
+                    <h4 className="DateFour">Jan 2025 - Apr 2025</h4>
+                </div>
+                <ul className="listOne">
+                    <li>
+                        Contributed to the full stack development of 3 web applications (2 public facing, 1 internal) for the Towing and Storage
+                        Safety and Enforcement Act project
+                    </li>
+                    <li>
+                        Angular, TypeScript, Java/Spring Boot, Azure DevOps, CI/CD, Azure Cloud, and REST APIs to
+                        successfully deploy 50 feature tickets that enhanced user experience for Ontario tow operators
+                    </li>
+                    <li>
+                        Worked on a Cyber Security Automation project that proactively scanned cloud applications to detect known CVEs and
+                        security vulnerabilities by developing automated security scanning scripts using Python (Playwright) and OWASP
+                        ZAP, integrated into Azure CI/CD pipelines
+                    </li>
+                    <li>
+                        Designed and executed comprehensive REST API test using Swagger and Postman
+                    </li>
+                </ul>
             </div>
-            <ul className="listOne">
-<li>
-Contributed to the full stack development of 3 web applications (2 public facing, 1 internal) for the Towing and Storage
-Safety and Enforcement Act project
-</li>
-<li>
-Angular, TypeScript, Java/Spring Boot, Azure DevOps, CI/CD, Azure Cloud, and REST APIs to
-successfully deploy 50 feature tickets that enhanced user experience for Ontario tow operators
-</li>
-<li>
-Worked on a Cyber Security Automation project that proactively scanned cloud applications to detect known CVEs and
-security vulnerabilities by developing automated security scanning scripts using Python (Playwright) and OWASP
-ZAP, integrated into Azure CI/CD pipelines
-</li>
-<li>
-Designed and executed comprehensive REST API test using Swagger and Postman
-</li>
-            </ul>
-        </div>
-        <div >
-            <div className="JobOne">
-                <h3 className="TheScore">Quality Assurance Analyst @ theScore</h3>
-                <h4 className="DateOne">Jan 2024 - Aug 2024</h4>
-            </div>
-            <ul className="listOne">
 
-                <li>Worked on 17 releases for the wagering team, performing comprehensive regression, backward compatibility, migration,
-and production smoke testing across multiple environments (UAT, Demo, Staging) and platforms (Android, iOS, web) for
-theScore Bet and ESPN, covering approximately 3,000 test cases</li>
-            <li>Identified production bugs, creating 5 new functional tickets on JIRA and Confluence, and signed off on approximately 140
-functional tickets, including 2 feature enhancements related to market logo rebranding and home page market
-recommendations. Developed new test cases in TestRail and delivered detailed reports</li>
+            <div>
+                <div className="JobOne">
+                    <h3 className="TheScore">Quality Assurance Analyst @ theScore</h3>
+                    <h4 className="DateOne">Jan 2024 - Aug 2024</h4>
+                </div>
+                <ul className="listOne">
+                    <li>Worked on 17 releases for the wagering team, performing comprehensive regression, backward compatibility, migration,
+                        and production smoke testing across multiple environments (UAT, Demo, Staging) and platforms (Android, iOS, web) for
+                        theScore Bet and ESPN, covering approximately 3,000 test cases</li>
+                    <li>Identified production bugs, creating 5 new functional tickets on JIRA and Confluence, and signed off on approximately 140
+                        functional tickets, including 2 feature enhancements related to market logo rebranding and home page market
+                        recommendations. Developed new test cases in TestRail and delivered detailed reports</li>
+                    <li>Participated in weekly stand-ups, occasionally leading sessions, and provided cross-departmental support by offering
+                        clarification on wagering-related issues</li>
+                </ul>
+            </div>
 
-            <li>Participated in weekly stand-ups, occasionally leading sessions, and provided cross-departmental support by offering
-clarification on wagering-related issues</li>
-            </ul>
-        </div>
-        <div >
-            <div className="JobThree">
-                <h3 className="VC">Quality Assurance Analyst @ Venture from Canada (Client: TAMVOES)</h3>
-                <h4 className="DateThree">Sept 2023 – Oct 2023</h4>
+            <div>
+                <div className="JobThree">
+                    <h3 className="VC">Quality Assurance Analyst @ Venture from Canada (Client: TAMVOES)</h3>
+                    <h4 className="DateThree">Sept 2023 – Oct 2023</h4>
+                </div>
+                <ul className="listThree">
+                    <li>Performed quality assurance across all three platforms (Android, iOS, web) by validating login processes, updating authentication headers, and verifying time zone configurations to ensure platform stability</li>
+                    <li>Compiled detailed end user analysis reports with actionable UI/UX improvements recommendations, demonstrating strong analytical and documentation skills</li>
+                </ul>
             </div>
-            <ul className="listThree">
-                <li>* Performed quality assurance across all three platforms (Android, iOS, web) by validating login processes, updating authentication headers, and verifying time zone configurations to ensure platform stability</li>
-                <li>Compiled detailed end user analysis reports with actionable UI/UX improvements recommendations, demonstrating strong analytical and documentation skills </li>
-                
-            </ul>
-        </div>
-        <div >
-            <div className="JobTwo">
-                <h3 className="VC">Software Developer @ Venture from Canada (Client: MyHomeworkRewards)</h3>
-                <h4 className="DateTwo">May 2023 – Jun 2023</h4>
+
+            <div>
+                <div className="JobTwo">
+                    <h3 className="VC">Software Developer @ Venture from Canada (Client: MyHomeworkRewards)</h3>
+                    <h4 className="DateTwo">May 2023 – Jun 2023</h4>
+                </div>
+                <ul className="listTwo">
+                    <li>Designed and developed a set of 100 math practice problems, coding each one directly into PHP for a cohort of 200 Grades 11 and 12 students an interactive, web-based platform using PHP to deliver math practice problems to students, enhancing
+                        engagement and accessibility</li>
+                    <li>Collaborated with my manager to ensure all content was meticulously aligned with the Toronto District School Board (TDSB) curriculum</li>
+                </ul>
             </div>
-            <ul className="listTwo">
-                <li>Designed and developed a set of 100 math practice problems, coding each one directly into PHP for a cohort of 200 Grades 11 and 12 students an interactive, web-based platform using PHP to deliver math practice problems to students, enhancing
-engagement and accessibility</li>
-                <li>Collaborated with my manager to ensure all content was meticulously aligned with the Toronto District School Board (TDSB) curriculum </li>
-            </ul>
-        </div>
-        <div >
-            <div className="JobThree">
-                <h3 className="Ruii">Software Developer @ Venture from Canada (Client: Ruii)</h3>
-                <h4 className="DateThree">Mar 2023 – Apr 2023</h4>
+
+            <div>
+                <div className="JobThree">
+                    <h3 className="Ruii">Software Developer @ Venture from Canada (Client: Ruii)</h3>
+                    <h4 className="DateThree">Mar 2023 – Apr 2023</h4>
+                </div>
+                <ul className="listThree">
+                    <li>Optimized two key web pages, including the "Get a Quote" and "Blog," by completing five high-priority tickets using React</li>
+                    <li>Worked on the design and development of a new company logo using Canvas</li>
+                    <li>Collaborated with design and product teams, ensuring seamless communication and delivering projects on time</li>
+                </ul>
             </div>
-            <ul className="listThree">
-                <li>Optimized two key web pages, including the "Get a Quote" and "Blog," by completing five high-priority tickets using React</li>
-                <li>Worked on the design and development of a new company logo using Canvas</li>
-                <li>Collaborated with design and product teams, ensuring seamless communication and delivering projects on time</li>
-            </ul>
+
         </div>
-    </div>);
+    );
 }
 export default Workexperience
